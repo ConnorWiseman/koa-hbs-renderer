0647f6761abe0d43ea1e863328aae53afe7c9583
Connor Wiseman <wiseman.connor@gmail.com>
2017-04-16 21:36:32 -0600

    Fixed cross-platform newline issue

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -15,6 +15,9 @@ chai.should();
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
+// Define local constants.
+const eol = (process.platform === 'win32' ? '\r\n' : '\n');
+
 // Require module to test.
 const renderer = require('../../index.js');
 
@@ -49,18 +52,18 @@ describe('Renderer', function() {
 
   it('should render the specified template', function(done) {
     request.get('/').expect(200).then(result => {
-      result.text.should.deep.equal(`<!DOCTYPE html>\r\n\
-<html lang="en-US">\r\n\
-    <head>\r\n\
-        <title>Title!</title>\r\n\
-        <meta charset="utf-8" />\r\n\
-    </head>\r\n\
-\r\n\
-    <body>\r\n\
-        <p>This is a template. Isn't that useful?</p>\n\n<p>This is a partial!</p>\r\n\
-\r\n\
-    </body>\r\n\
-</html>\r\n`);
+      result.text.should.equal(`<!DOCTYPE html>${eol}\
+<html lang="en-US">${eol}\
+    <head>${eol}\
+        <title>Title!</title>${eol}\
+        <meta charset="utf-8" />${eol}\
+    </head>${eol}\
+${eol}\
+    <body>${eol}\
+        <p>This is a template. Isn't that useful?</p>\n\n<p>This is a partial!</p>${eol}\
+${eol}\
+    </body>${eol}\
+</html>${eol}`);
       done();
     });
   });
